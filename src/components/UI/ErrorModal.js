6a0e63d5e5e4d18f17515c110089df8f7139929c
Amol shelke <shelkeamol897@gmail.com>
2022-11-30 19:46:20 +0530

    Moved the Backdrop and errorModal overlay outside of the root element using the createPortal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,48 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Button from './Button';
 import Card from './Card';
 
 import './ErrorModal.css';
 
+const BackDrop = props => {
+  return <div className="backdrop" onClick={props.onConfirm} />;
+};
+
+const ModelOverlay = props => {
+  return (
+    <Card className="modal">
+      <header className="header">
+        <h2>{props.errorTitle}</h2>
+      </header>
+      <div className="content">
+        <p>{props.errorMessage}</p>
+      </div>
+      <footer className="actions">
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = props => {
   return (
-    <div>
-      <div className="backdrop" onClick={props.onConfirm} />
-      <Card className="modal">
-        <header className="header">
-          <h2>{props.errorTitle}</h2>
-        </header>
-        <div className="content">
-          <p>{props.errorMessage}</p>
-        </div>
-        <footer className="actions">
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root'),
+      )}
+      ,
+      {ReactDOM.createPortal(
+        <ModelOverlay
+          errorTitle={props.errorTitle}
+          errorMessage={props.errorMessage}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root'),
+      )}
+    </React.Fragment>
   );
 };
 
